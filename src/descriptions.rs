ced3837889f9b05c9ffbe57083900e4d65768c5d
Daniel Collin <daniel@collin.com>
2016-11-27 12:33:24 +0100

    Added some more descriptions

--- src/descriptions.rs
+++ src/descriptions.rs
@@ -21,12 +21,28 @@ const FLAGS_ARC: FlagsDesc = FlagsDesc {
     c: Flag::Set("C — Set if a carry is generated; cleared otherwise."),
 };
 
+const FLAGS_X: FlagsDesc = FlagsDesc {
+    x: Flag::Set("X - Set the same as carry"),
+    n: Flag::Set("N - Set if the result is negative; cleared otherwise."),
+    z: Flag::Set("Z — Cleared if the result is non-zero; unchanged otherwise."),
+    v: Flag::Set("V — Set if an overflow is generated; cleared otherwise."),
+    c: Flag::Set("C — Set if a carry is generated; cleared otherwise."),
+};
+
 const FLAGS_ABCD: FlagsDesc = FlagsDesc {
     x: Flag::Set("X - Set the same as carry"),
     n: Flag::Undefined,
     z: Flag::Clear("Z — Cleared if the result is nonzero; unchanged otherwise"),
     v: Flag::Undefined,
     c: Flag::Set("C — Set if a decimal carry was generated; cleared otherwise."),
+};
+
+const FLAGS_AND: FlagsDesc = FlagsDesc {
+    x: Flag::NotAffected("X - Not Affected"),
+    n: Flag::Set("N — Set if the most significant bit of the result is set; cleared otherwise."),
+    z: Flag::Set("Z — Set if the result is zero; cleared otherwise."),
+    v: Flag::Clear("V — Always cleared."),
+    c: Flag::Clear("C — Always cleared."),
 };
 
 pub struct Description {
@@ -43,7 +59,7 @@ pub const ABCD_DESC: Description = Description {
         2. Memory to Memory: The operands are addressed with the predecrement addressing mode using the address registers specified in the instruction.\n
         This operation is a byte operation only.",
     operation: "Source10 + Destination10 + X → Destination",
-    assembler: &["Add < ea > ,Dn", "Add Dn, < ea >"],
+    assembler: &["abcd < ea > ,Dn", "Add Dn, < ea >"],
     attributes: "Byte, Word, Long",
     flags: &FLAGS_ABCD,
 };
@@ -55,4 +71,32 @@ pub const ADD_DESC: Description = Description {
     attributes: "Byte, Word, Long",
     flags: &FLAGS_ARC,
 };
+
+pub const ADDQ_DESC: Description = Description {
+    description: "Adds an immediate value of one to eight to the operand at the destination location. The size of the operation may be specified as byte, word, or long. Word and long operations are also allowed on the address registers. When adding to address registers, the condition codes are not altered, and the entire destination address register is used regardless of the operation size.",
+    operation: "Immidate + Destination → Destination",
+    assembler: &["addq # < data > , < ea >"],
+    attributes: "Byte, Word, Long",
+    flags: &FLAGS_ARC,
+};
+
+pub const ADDX_DESC: Description = Description {
+    description: "Adds the source operand and the extend bit to the destination operand and stores the result in the destination location. The operands can be addressed in two different ways:
+                    1. Data register to data register—The data registers specified in the instruction contain the operands.
+                    2. Memory to memory—The address registers specified in the instruction address the operands using the predecrement addressing mode.
+                    The size of the operation can be specified as byte, word, or long.";
+    operation: "Source + Destination + X → Destination",
+    assembler: &["addx Dy,Dx", "addx -(Ay),-(Ax)"],
+    attributes: "Byte, Word, Long",
+    flags: &FLAGS_X,
+};
+
+pub const AND_DESC: Description = Description {
+    description: "Performs an AND operation of the source operand with the destination operand and stores the result in the destination location. The size of the operation can be specified as byte, word, or long. The contents of an address register may not be used as an operand.",
+    operation: "Source & Destination → Destination",
+    assembler: &["and < ea > ,Dn", "Add Dn, < ea >"],
+    attributes: "Byte, Word, Long",
+    flags: &FLAGS_AND,
+};
+
 
