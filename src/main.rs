2e2937a15563d352a0ade9bd83681c9c88159946
Daniel Collin <daniel@collin.com>
2016-12-22 13:20:59 +0100

    Merged the separate passes into one

--- src/main.rs
+++ src/main.rs
@@ -47,9 +47,11 @@ impl Op {
         }
     }
 }
-struct Instruction {
+struct Instruction<'a> {
     name: &'static str,
     desc: Option<Description>,
+    matrix: &'a [&'a [Op]],
+    override_imm: Option<usize>,
 }
 
 fn compile_statement(filename: &str, file_out: &str, statement: &str) -> bool {
@@ -271,6 +273,113 @@ fn generate_table(name: &str,
     }
 }
 
+fn generate_statements_two_args(name: &str, inst: &Instruction, is_long: bool) -> Vec<BuildResult> {
+    let mut statements = Vec::with_capacity(20 * 20);
+    let mut count = 0;
+
+    for src in inst.matrix[0] {
+        for dst in inst.matrix[1] {
+            let file_in = format!("target/temp_{}.s", count);
+            let file_out = format!("target/temp_{}.o", count);
+
+            statements.push(BuildResult {
+                src: Some(src.clone()),
+                dst: dst.clone(),
+                temp_file: file_in,
+                temp_out: file_out,
+                cycle_count: None,
+            });
+
+            count += 1;
+        }
+    }
+
+    statements.par_iter_mut().weight_max().for_each(|v| {
+        let src = v.src.unwrap();
+        let statement;
+
+        if src.print_name == "#xxx" && is_long {
+            statement = format!("{} {},{}", name, "#$ffffff", v.dst.name);
+        } else {
+            statement = format!("{} {},{}", name, v.src.unwrap().name, v.dst.name);
+        }
+        
+        if compile_statement(&v.temp_file, &v.temp_out, &statement) {
+            v.cycle_count = Some(0); // indicate that this should be processed
+        }
+    });
+
+    statements
+}
+
+fn generate_statements_one_arg(name: &str, inst: &Instruction, is_long: bool) -> Vec<BuildResult> {
+    let mut statements = Vec::with_capacity(20 * 20);
+    let mut count = 0;
+
+    for dst in inst.matrix[0] {
+        let file_in = format!("target/temp_{}.s", count);
+        let file_out = format!("target/temp_{}.o", count);
+
+        statements.push(BuildResult {
+            src: None,
+            dst: dst.clone(),
+            temp_file: file_in,
+            temp_out: file_out,
+            cycle_count: None,
+        });
+
+        count += 1;
+    }
+
+    statements.par_iter_mut().weight_max().for_each(|v| {
+        let statement = format!("{} {}", name, v.dst.name);
+        println!("one arg statement {}", statement);
+        if compile_statement(&v.temp_file, &v.temp_out, &statement) {
+            v.cycle_count = Some(0);
+        }
+    });
+
+    statements
+}
+
+fn generate_statements_no_args(name: &str) -> Vec<BuildResult> {
+    let mut statement = Vec::with_capacity(1);
+    let file_in = format!("target/temp_{}.s", 0);
+    let file_out = format!("target/temp_{}.o", 0);
+
+    statement.push(BuildResult {
+        src: None,
+        dst: Op::new("", ""),
+        temp_file: "".to_owned(), 
+        temp_out:  "".to_owned(),
+        cycle_count: None,
+    });
+
+    if compile_statement(&file_in, &file_out, name) {
+        statement[0].cycle_count = Some(0);
+    }
+
+    statement
+}
+
+fn generate_table_2(name: &str, inst: &Instruction, is_long: bool) {
+    let mut statements;
+
+    if inst.matrix.len() == 2 {
+        statements = generate_statements_two_args(name, inst, is_long);
+        compile_cycle_counts(&mut statements);
+        print_grid_table(name, &statements, inst.matrix[0], inst.matrix[1]);
+    } else if inst.matrix.len() == 1 {
+        statements = generate_statements_one_arg(name, inst, is_long);
+        compile_cycle_counts(&mut statements);
+        print_table(name, &statements, inst.matrix[0]);
+    } else {
+        statements = generate_statements_no_args(name);
+    }
+
+
+}
+
 fn main() {
     let dest_types = [
         Op::new("d0", "Dn"),
@@ -297,7 +406,14 @@ fn main() {
         Op::new("2(pc,d0)", "d(PC,Dn)"),
         Op::new("#2000", "#xxx")];
 
-    let dest_types_none = [Op::new("", "")];
+    //let dest_types_none = [Op::new("", "")];
+
+    //let mut two_ops = Vec::<&[Op]>::new();
+    let two_ops: &[&[Op]] = &[&src_types, &dest_types];
+    let one_op: &[&[Op]] = &[&dest_types];
+
+    //two_ops.push(&src_types);
+    //two_ops.push(&dest_types);
 
     unsafe {
         m68k_wrapper_init();
@@ -305,94 +421,127 @@ fn main() {
 
     let inst_2_ops_000 =
         [
+        /*
         Instruction {
             name: "abcd",
             desc: Some(ABCD_DESC),
+            matrix: two_ops,
+            override_imm: None,
         },
+        */
 
         Instruction {
             name: "add",
             desc: Some(ADD_DESC),
+            matrix: two_ops,
+            override_imm: None,
         },
+        Instruction {
+            name: "clr",
+            desc: Some(CLR_DESC),
+            matrix: one_op,
+            override_imm: None,
+        },
+        /*
         Instruction {
             name: "addq",
             desc: Some(ADDQ_DESC),
+            .. Instruction::default()
         },
         Instruction {
             name: "addx",
             desc: Some(ADDX_DESC),
+            .. Instruction::default()
         },
         Instruction {
             name: "and",
             desc: Some(AND_DESC),
+            .. Instruction::default()
         },
         Instruction {
             name: "bchg",
             desc: Some(BCHG_DESC),
+            .. Instruction::default()
         },
         Instruction {
             name: "bclr",
             desc: Some(BCLR_DESC),
+            .. Instruction::default()
         },
         Instruction {
             name: "bset",
             desc: Some(BSET_DESC),
+            .. Instruction::default()
         },
         Instruction {
             name: "btst",
             desc: Some(BTST_DESC),
+            .. Instruction::default()
         },
         Instruction {
             name: "cmp",
             desc: Some(CMP_DESC),
+            .. Instruction::default()
         },
         Instruction {
             name: "divu",
             desc: Some(DIVS_DIVU_DESC),
+            .. Instruction::default()
         },
         Instruction {
             name: "divs",
             desc: Some(DIVS_DIVU_DESC),
+            .. Instruction::default()
         },
         Instruction {
             name: "eor",
             desc: Some(EOR_DESC),
+            .. Instruction::default()
         },
         Instruction {
             name: "exg",
             desc: Some(EXG_DESC),
+            .. Instruction::default()
         },
         Instruction {
             name: "lea",
             desc: Some(LEA_DESC),
+            .. Instruction::default()
         },
         Instruction {
             name: "move",
             desc: Some(MOVE_DESC),
+            .. Instruction::default()
         },
         Instruction {
             name: "muls",
             desc: Some(MULS_DESC),
+            .. Instruction::default()
         },
         Instruction {
             name: "mulu",
             desc: Some(MULU_DESC),
+            .. Instruction::default()
         },
         Instruction {
             name: "or",
             desc: Some(OR_DESC),
+            .. Instruction::default()
         },
         Instruction {
             name: "sub",
             desc: Some(SUB_DESC),
+            .. Instruction::default()
         },
         Instruction {
             name: "subq",
             desc: Some(SUBQ_DESC),
+            .. Instruction::default()
         },
         Instruction {
             name: "subx",
             desc: Some(SUBX_DESC),
+            .. Instruction::default()
         },
         ];
 
@@ -400,63 +549,82 @@ fn main() {
         Instruction {
             name: "clr",
             desc: Some(CLR_DESC),
+            .. Instruction::default()
         },
         Instruction {
             name: "ext",
             desc: Some(EXT_DESC),
+            .. Instruction::default()
         },
         Instruction {
             name: "bsr",
             desc: Some(BSR_DESC),
+            .. Instruction::default()
         },
         Instruction {
             name: "jsr",
             desc: Some(JSR_DESC),
+            .. Instruction::default()
         },
         Instruction {
             name: "jmp",
             desc: Some(JMP_DESC),
+            .. Instruction::default()
         },
         Instruction {
             name: "neg",
             desc: Some(NEG_DESC),
+            .. Instruction::default()
         },
         Instruction {
             name: "not",
             desc: Some(NOT_DESC),
+            .. Instruction::default()
         },
         Instruction {
             name: "swap",
             desc: Some(SWAP_DESC),
+            .. Instruction::default()
         },
+        */
         ];
 
+    /*
     let inst_0_ops_000 = [
         Instruction {
             name: "nop",
             desc: Some(NOP_DESC),
+            .. Instruction::default()
         },
         Instruction {
             name: "illegal",
             desc: None,
+            .. Instruction::default()
         },
         Instruction {
             name: "rte",
             desc: Some(RTS_DESC),
+            .. Instruction::default()
         },
         Instruction {
             name: "rts",
             desc: Some(RTS_DESC),
+            .. Instruction::default()
         },
     ];
+    */
 
 
     for inst in &inst_2_ops_000 {
         let name_long = format!("{}.l", inst.name);
 
         print_instruction_header(inst);
-        generate_table(inst.name, false, Some(&src_types), &dest_types);
-        generate_table(&name_long, true, Some(&src_types), &dest_types);
+        generate_table_2(inst.name, &inst, false);
+        generate_table_2(&name_long, &inst, true);
+
+
+        //generate_table(inst.name, false, Some(&src_types), &dest_types);
+        //generate_table(&name_long, true, Some(&src_types), &dest_types);
     }
 
     // Generate instructions with one op
